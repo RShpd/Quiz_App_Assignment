285952315c1af408f6d1aa5798a94d361f511210
Shahpasand <ra.shahpasand@gmail.com>
2022-01-07 16:59:24 +0330

    Update detail_page.dart

--- lib/presentation/detail/detail_page.dart
+++ lib/presentation/detail/detail_page.dart
@@ -28,64 +28,22 @@ class DetailPage extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Text(
-                      quiz.title,
-                      style: Theme.of(context).textTheme.headline6,
+                    const Text(
+                      'Please select the correct answers and at the enf hit the submit button.',
+                      textScaleFactor: 1.2,
                     ),
                     ListView.builder(
-                      padding: const EdgeInsets.symmetric(vertical: 6,),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 6,
+                      ),
                       shrinkWrap: true,
                       physics: const ScrollPhysics(),
                       itemCount: quiz.questions.length,
                       itemBuilder: (context, index) {
                         final Question q = quiz.questions[index];
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 4),
-                          child: Card(
-                            color: quiz.color.withOpacity(0.4),
-                            shadowColor: Colors.transparent,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(17),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding:
-                                        const EdgeInsets.symmetric(vertical: 5),
-                                    child: Text(
-                                      q.question,
-                                      textScaleFactor: 1.3,
-                                      textAlign: TextAlign.center,
-                                      style: Theme.of(context).textTheme.subtitle2,
-                                    ),
-                                  ),
-                                  RadioListTile(
-                                    title: Text(q.possibleAnswers[1]),
-                                    contentPadding: EdgeInsets.zero,
-                                    value: q.possibleAnswers[1],
-                                    groupValue: '',
-                                    onChanged: (option) {},
-                                  ),
-                                  RadioListTile(
-                                    title: Text(q.possibleAnswers[2]),
-                                    value: q.possibleAnswers[2],
-                                    contentPadding: EdgeInsets.zero,
-                                    groupValue: '',
-                                    onChanged: (option) {},
-                                  ),
-                                  RadioListTile(
-                                    title: Text(q.possibleAnswers[3]),
-                                    value: q.possibleAnswers[3],
-                                    contentPadding: EdgeInsets.zero,
-                                    groupValue: '',
-                                    onChanged: (option) {},
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+                        return RadioListTileWidget(
+                          question: q,
+                          color: quiz.color,
                         );
                       },
                     ),
@@ -94,6 +52,71 @@ class DetailPage extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class RadioListTileWidget extends StatefulWidget {
+  final Color color;
+  final Question question;
+
+  const RadioListTileWidget(
+      {Key? key, required this.color, required this.question})
+      : super(key: key);
+
+  @override
+  _RadioListTileWidgetState createState() => _RadioListTileWidgetState();
+}
+
+class _RadioListTileWidgetState extends State<RadioListTileWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Card(
+        color: widget.color.withOpacity(0.4),
+        shadowColor: Colors.transparent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(17),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Text(
+                  widget.question.question,
+                  textScaleFactor: 1.3,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.subtitle2,
+                ),
+              ),
+              RadioListTile(
+                title: Text(widget.question.possibleAnswers[1]),
+                contentPadding: EdgeInsets.zero,
+                value: widget.question.possibleAnswers[1],
+                groupValue: '',
+                onChanged: (option) {},
+              ),
+              RadioListTile(
+                title: Text(widget.question.possibleAnswers[2]),
+                value: widget.question.possibleAnswers[2],
+                contentPadding: EdgeInsets.zero,
+                groupValue: '',
+                onChanged: (option) {},
+              ),
+              RadioListTile(
+                title: Text(widget.question.possibleAnswers[3]),
+                value: widget.question.possibleAnswers[3],
+                contentPadding: EdgeInsets.zero,
+                groupValue: '',
+                onChanged: (option) {},
+              ),
+            ],
+          ),
         ),
       ),
     );
